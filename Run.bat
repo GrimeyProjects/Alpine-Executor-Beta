fcac994e31eaabb1953189951bd1034a8b7cfb0e
Troller <154270907+DataSnipes@users.noreply.github.com>
2025-04-07 10:15:26 -0600

    Create Run

---
+++ Run.bat
@@ -0,0 +1,33 @@
+      / \       | |           ___________    _   __       _    ________
+     / _ \      | |          |   ______  |  | | |   \    | |  |   _____|
+    / / \ \     | |          |  |______| |  | | | |\ \   | |  |  |___
+   / /___\ \    | |          | __________|  | | | | \ \  | |  |   ___|
+  / _______ \   | |          | |            | | | |  \ \ | |  |  |
+ / /       \ \  | |________  | |            | | | |   \ \| |  |  |______
+/_/         \_\ |__________| |_|            |_| |_|    \___|  |_________|
+
+Created By GrimeyBanana
+[Version:1.0.0.0]
+(Patch Notes: https://datasnipes.github.io/Alpine-Patch)
+(Help: https://datasnipes.github.io/alpine-help)
+
+WARNING: This Is A Beta Version, Things Might Break!.
+
+Please Wait.........
+
+Patching.........
+
+Getting Packages.........
+
+Checking For Updates...........
+
+Running Dependancys...........
+
+Success!
+
+Please Type A Shortcut Name (Must Be A URL)
+
+
+@echo off
+
+Start /Programs/Shortcuts/${message}.Url
